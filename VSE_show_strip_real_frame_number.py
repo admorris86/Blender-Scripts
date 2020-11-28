e577793c449a7595d8ed3c0b0fb80bb77cb645b8
Adam Morris <ad.morris@live.co.uk>
2020-11-28 23:35:06 +0000

    Update VSE_show_strip_real_frame_number.py

--- VSE_show_strip_real_frame_number.py
+++ VSE_show_strip_real_frame_number.py
@@ -1,3 +1,26 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+### Known Issues ###
+
+- Deleting the selected scene strip throws up an error: The code needs
+conditionals adding to to allow for this.
+
+- Strips modified by the Speed Controller effect don't take the effect
+into account: This is potentially something that could be added.
+
+"""
 import bpy
 import os
 
