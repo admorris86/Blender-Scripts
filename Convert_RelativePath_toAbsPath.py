a6a43ba94848315518634772a851dc149e0ff27b
Adam Morris <ad.morris@live.co.uk>
2020-12-01 23:05:56 +0000

    Update Convert_RelativePath_toAbsPath.py

--- Convert_RelativePath_toAbsPath.py
+++ Convert_RelativePath_toAbsPath.py
@@ -1,27 +1,9 @@
-"""
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-### About This Script ###
-This script will convert any relative path in Blender (on a Windows
-system) to and absolute path. Modify and run it in the console to return
-the output.
-
-'''
 import bpy
 from pathlib import Path
 
 directoryPath = bpy.path.abspath("//")
 
-relativePath = r"" # insert relative path here, keep the r!
+relativePath = r"" # insert relative path here, keep the r
 
 # count parent directories above relative path
 parentDirs = relativePath.count("..\\")
