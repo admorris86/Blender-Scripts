5af08968ee3a39b36e3f83ebe7c6ceb30e89d7c1
Adam Morris <ad.morris@live.co.uk>
2020-12-06 22:44:18 +0000

    Update Convert_RelativePath_toAbsPath.py

--- Convert_RelativePath_toAbsPath.py
+++ Convert_RelativePath_toAbsPath.py
@@ -1,33 +1,44 @@
-import bpy
-from pathlib import Path
+def absolutePath(relativePath):
+    '''
+    input a relative path from THIS blend file to return 
+    it as an absolute path. Note to use the raw string tag on Windows 
+    i.e. absolutePath(r"mystring") to ensure the \ characters are 
+    escaped properly
+    '''
+       
+    import bpy
+    from pathlib import Path
 
-directoryPath = bpy.path.abspath("//")
+    directoryPath = bpy.path.abspath("//")
+    
+    # count parent directories above relative path
+    parentDirs = relativePath.count("..\\")
+    
+    if parentDirs > 0:
+    
+        # assign this blend file's full directory path to a new variable
+        # named parentPath
+        parentPath = Path(directoryPath)
 
-relativePath = r"" # insert relative path here, keep the r
+        # for the number of parent directories above this blend,
+        # re-assign the value of parentPath to the path of the
+        # directory above i.e step up x levels
+        for k in range(parentDirs):
+            parentPath = parentPath.parent
 
-# count parent directories above relative path
-parentDirs = relativePath.count("..\\")
+        # trim relative path from the first char after the relative path symbols
+        # i.e keep anything after this stuff //..\..\
+        i = 0
+        for char in relativePath:
+            if char in ['\\', '/', '.']:
+                i += 1
+            else:
+                relativePath = "\\" + relativePath[i:]
+                # combined parentPath and trimmed relative path for the full path
+                fullPath = str(parentPath) + relativePath
+                break
+        
+    else: 
+        fullPath = relativePath
 
-# assign this blend file's full directory path to a new variable
-# named parentPath
-parentPath = Path(directoryPath)
-
-# for the number of parent directories above this blend,
-# re-assign the value of parentPath to the path of the
-# directory above i.e step up x levels
-for k in range(parentDirs):
-    parentPath = parentPath.parent
-
-# trim relative path from the first char after the relative path symbols
-# i.e keep anything after this stuff //..\..\
-i = 0
-for char in relativePath:
-    if char in ['\\', '/', '.']:
-        i += 1
-    else:
-        relativePath = "\\" + relativePath[i:]
-        # combined parentPath and trimmed relative path for the full path
-        fullPath = str(parentPath) + relativePath
-        break
-
-print(fullPath)
+    return fullPath
